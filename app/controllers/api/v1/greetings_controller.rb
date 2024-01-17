ca6f7bbe7359970ced3089b9173aae885539c96e
Garvin Chimone <106396944+Chimoneg27@users.noreply.github.com>
2024-01-17 20:23:32 +0200

    Fix linter errors

--- app/controllers/api/v1/greetings_controller.rb
+++ app/controllers/api/v1/greetings_controller.rb
@@ -1,9 +1,9 @@
 class Api::V1::GreetingsController < ApplicationController
   def index
     greetings = Greeting.all
-    
+
     if greetings
-      render json: {status: 'SUCCESS', message: 'Loaded greetings', data: greetings}, status: :ok
+      render json: { status: 'SUCCESS', message: 'Loaded greetings', data: greetings }, status: :ok
     else
       render json: greetings.errors, status: :bad_request
     end
